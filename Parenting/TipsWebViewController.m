622fe31d0ce7a2e8cb6b600bfec71f9afdd9968c
Jamming <arvilinam@gmail.com>
2013-11-28 17:46:45 +0800

    v1.3.2 1127

--- Parenting/TipsWebViewController.m
+++ Parenting/TipsWebViewController.m
@@ -88,8 +88,9 @@
     if (count < 5) {
         MBProgressHUD *hud = [MBProgressHUD showHUDAddedTo:self.view animated:YES];
         hud.mode = MBProgressHUDModeIndeterminate;
-        hud.alpha = 1;
-        hud.labelText = @"Loading...";
+        hud.alpha = 0.5;
+        hud.color = [UIColor grayColor];
+        hud.labelText = NSLocalizedString(@"PlotLoading", nil);
         //random color
 //        CGFloat red =  arc4random_uniform(256)/255.f;
 //        CGFloat blue = arc4random_uniform(256)/255.f;
