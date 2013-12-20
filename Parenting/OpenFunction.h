df47237442443884bdef8c819a9ed5588b7fcdfd
Jamming <arvilinam@gmail.com>
2013-12-20 14:26:21 +0800

    1.4.0 release

--- Parenting/OpenFunction.h
+++ Parenting/OpenFunction.h
@@ -3,7 +3,7 @@
 //  Amoy Baby Care
 //
 //  Created by @Arvi@ on 13-9-23.
-//  Copyright (c) 2013年 奥芬多. All rights reserved.
+//  Copyright (c) 2013年 爱摩信息科技. All rights reserved.
 //
 
 #import <Foundation/Foundation.h>
@@ -14,5 +14,14 @@
 + (void) openUserReviews;
 + (NSString*)getpm25description:(int)value;
 + (NSString*)getopenudid;
++ (NSString*)getMonthBeginAndEndWith:(NSDate *)newDate;
++ (NSString*)getWeekBeginAndEndWith:(NSDate *)newDate;
++ (long)getTimeStampFromDate:(NSDate*)date;
++ (NSDate*)getDateFromTimeStamp:(long)timestamp;
 
++(void)addLocalNotificationWithMessage:(NSString *)message
+                              FireDate:(NSDate *) fireDate
+                              AlarmKey:(NSString *)alarmKey;
+
++(void)deleteLocalNotification:(NSString *) alarmKey;
 @end
--- Parenting/OpenFunction.m
+++ Parenting/OpenFunction.m
@@ -3,7 +3,7 @@
 //  Amoy Baby Care
 //
 //  Created by @Arvi@ on 13-9-23.
-//  Copyright (c) 2013年 奥芬多. All rights reserved.
+//  Copyright (c) 2013年 爱摩信息科技. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
@@ -63,4 +63,104 @@
     return [APService openUDID];
 }
 
++ (NSString*)getWeekBeginAndEndWith:(NSDate *)newDate{
+    NSLog(@"%@",newDate);
+    if (newDate == nil) {
+        newDate = [NSDate date];
+    }
+    double interval = 0;
+    NSDate *beginDate = nil;
+    NSDate *endDate = nil;
+    
+    NSCalendar *calendar = [NSCalendar currentCalendar];
+    [calendar setFirstWeekday:1];//设定周日为周首日
+    BOOL ok = [calendar rangeOfUnit:NSWeekCalendarUnit startDate:&beginDate interval:&interval forDate:newDate];
+    //分别修改为 NSDayCalendarUnit NSWeekCalendarUnit NSYearCalendarUnit
+    if (ok) {
+        endDate = [beginDate dateByAddingTimeInterval:interval-1];
+    }else {
+        return nil;
+    }
+    NSDateFormatter *myDateFormatter = [[NSDateFormatter alloc] init];
+    [myDateFormatter setDateFormat:@"MM.dd"];
+    NSString *beginString = [myDateFormatter stringFromDate:beginDate];
+    NSString *endString = [myDateFormatter stringFromDate:endDate];
+    
+    return [NSString stringWithFormat:@"%@~%@",beginString,endString];
+}
+
++ (NSString*)getMonthBeginAndEndWith:(NSDate *)newDate{
+    if (newDate == nil) {
+        newDate = [NSDate date];
+    }
+    double interval = 0;
+    NSDate *beginDate = nil;
+    NSDate *endDate = nil;
+    
+    NSCalendar *calendar = [NSCalendar currentCalendar];
+    [calendar setFirstWeekday:1];//设定周日为周首日
+    BOOL ok = [calendar rangeOfUnit:NSMonthCalendarUnit startDate:&beginDate interval:&interval forDate:newDate];
+    //分别修改为 NSDayCalendarUnit NSWeekCalendarUnit NSYearCalendarUnit
+    if (ok) {
+        endDate = [beginDate dateByAddingTimeInterval:interval-1];
+    }else {
+        return nil;
+    }
+    NSDateFormatter *myDateFormatter = [[NSDateFormatter alloc] init];
+    [myDateFormatter setDateFormat:@"yyyy.MM.dd"];
+    NSString *beginString = [myDateFormatter stringFromDate:beginDate];
+    NSString *endString = [myDateFormatter stringFromDate:endDate];
+    
+    return [NSString stringWithFormat:@"%@~%@",beginString,endString];
+}
+
++(long)getTimeStampFromDate:(NSDate*)date
+{
+    return [[NSString stringWithFormat:@"%ld", (long)[date timeIntervalSince1970]] intValue];
+}
+
++(NSDate*)getDateFromTimeStamp:(long)timestamp
+{
+       return [NSDate dateWithTimeIntervalSince1970:(NSTimeInterval)timestamp];
+}
+
+//添加本地通知的方法
+/*
+ message:显示的内容
+ firedate:闹钟的时间
+ alarmKey:闹钟的ID
+ */
++(void)addLocalNotificationWithMessage:(NSString *)message
+                              FireDate:(NSDate *) fireDate
+                              AlarmKey:(NSString *)alarmKey
+{
+    UILocalNotification *notification=[[UILocalNotification alloc] init];
+    if (notification!=nil) {
+        
+        notification.fireDate=fireDate;
+        
+        notification.timeZone=[NSTimeZone defaultTimeZone];
+        notification.soundName= UILocalNotificationDefaultSoundName;
+        
+        notification.alertBody=message;
+        notification.hasAction = NO;
+        notification.userInfo=[[NSDictionary alloc] initWithObjectsAndKeys:alarmKey,@"AlarmKey", nil];
+        [[UIApplication sharedApplication] scheduleLocalNotification:notification];
+    }
+}
+
+/*
+ 删除本地通知
+ */
++(void)deleteLocalNotification:(NSString *) alarmKey
+{
+    NSArray * allLocalNotification=[[UIApplication sharedApplication] scheduledLocalNotifications];
+    
+    for (UILocalNotification * localNotification in allLocalNotification) {
+        NSString * alarmValue=[localNotification.userInfo objectForKey:@"AlarmKey"];
+        if ([alarmKey isEqualToString:alarmValue]) {
+            [[UIApplication sharedApplication] cancelLocalNotification:localNotification];
+        }
+    }
+}
 @end
